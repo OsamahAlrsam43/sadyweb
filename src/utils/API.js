4a9af7828ae60610836e0b7ea15b3beaded8bf91
OsamahAlrsam43 <38662642+OsamahAlrsam43@users.noreply.github.com>
2020-12-02 10:41:56 +0300

    V
    V

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "http://35.224.38.29/api/v1/",
   responseType: "json",
   headers: {
     "Content-type": "application/json",
